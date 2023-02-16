1b544402853f4bfc15a62a11cb8ed777bd563e8b
Nestan <nestankeneshova16@gmail.com>
2023-02-16 14:41:12 +0600

    get all the elements of the array

--- index.js
+++ index.js
@@ -10,3 +10,11 @@ console.log(example1[4]);
 console.log(example1[7]);
 
 console.log(example2);//выв-т все элементы
+console.log(example2[1][1]);
+
+let test = example2[2];
+console.log(test[1]);
+//выв все эл массива
+for(let i = 0; i < example1.length; i++){
+  console.log(example1[i]);
+}
