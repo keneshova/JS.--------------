3205d279dfee20d23cac1be9649db6178e00574e
Nestan <nestankeneshova16@gmail.com>
2023-02-16 14:24:46 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,10 @@
+let example1 = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+let example2 = [ [1, 2, 3], [4, 5, 6], [7, 8, 9]];//сос-т из трех элементов
+
+console.log(example1.length);//length-это количество цифр выв-т сколько их
+console.log(example2.length);
+console.log(example1[2]);
+console.log(example2[2]);
+
+console.log(example1[4]);
+console.log(example1[7]);
